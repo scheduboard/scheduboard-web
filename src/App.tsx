f310b99d54f60213d477b80661ea875953b27327
masashi.fujii <megane.date@gmail.com>
2024-04-07 10:47:42 +0900

    環境構築修正-一旦デプロイできる状態

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        test
         <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
