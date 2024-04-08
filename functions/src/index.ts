2966b03203963b597bab5f21fab5f4586e009d5a
masashi.fujii <megane.date@gmail.com>
2024-04-08 23:55:18 +0900

    rest api settings

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,15 +5,15 @@ const app = express();
 const router = require('./router/index');
 app.use('/', router);
 
-import { onRequest } from 'firebase-functions/v2/https';
-import * as logger from 'firebase-functions/logger';
+// import { onRequest } from 'firebase-functions/v2/https';
+// import * as logger from 'firebase-functions/logger';
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info('Hello logs!', { structuredData: true });
+//   response.send('Hello from Firebase!');
+// });
 
 exports.api = functions.region('asia-northeast1').https.onRequest(app);
--- functions/src/router/index.ts
+++ functions/src/router/index.ts
@@ -1,36 +1,114 @@
 import { Router, Request, Response } from 'express';
+
+import {
+  DocumentData
+} from "firebase/firestore";
+
+const {
+  initializeApp,
+} = require("firebase-admin/app");
+
+const {
+  credential,
+} = require("firebase-admin");
+
+const {
+  getFirestore,
+} = require("firebase-admin/firestore");
+
+const serviceAccountFile = require('../../serviceAccountFile.json')
+
 const router = Router();
+
+initializeApp({
+  credential: credential.cert(serviceAccountFile),
+  databaseURL: 'https://sheduboard-default-rtdb.firebaseio.com'
+});
 
 const endPoint = '/';
 
+const db = getFirestore();
+
+type User = {
+  name: string;
+  age: number;
+  id: number;
+}
+
 router
   .route(endPoint)
-  .get((req: Request, res: Response) => {
+  .get(async (req: Request, res: Response) => {
     // ここにコレクション等の取得を書く
+    const messages = [] as User[]
+    try {
+      const querySnapshot = await db.collection('users').get();
+      querySnapshot.forEach((doc: DocumentData) => {
+        messages.push({
+          ...doc.data()
+        })
+      });
+    } catch (error) {
+      throw new Error;
+    }
     res.json({
       message: 'call by the GET method',
+      messages
     });
   })
-  .post((req: Request, res: Response) => {
-    // ここデータ送信の処理を書く
-    res.json({
-      message: 'call by the POST method',
-    });
+  .post(async (req: Request, res: Response) => {
+    try {
+      const { name, id, age } = req.body
+      console.log("req.body++++++++++", req.body)
+      const createdAt = new Date().toISOString() as string;
+
+      const docRef = await db.collection('users').add({
+        name, id, age, createdAt
+      })
+      const docSnapShot = await docRef.get()
+
+      const createMessage = {
+        id: docSnapShot.id,
+        ...docSnapShot.data()
+      }
+      res.json({
+        message: 'call by the POST method',
+        data: createMessage
+      });
+    } catch (error) {
+      throw new Error;
+    }
   });
 
 router
-  .route(`${endPoint}/:id`)
-  .put((req: Request, res: Response) => {
-    // ここデータ更新の処理を書く
-    res.json({
-      message: 'call by the PUT method ID:' + req.params.id,
-    });
+  .route(`${endPoint}:id`)
+  .put(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, age } = req.body
+    const newData = {
+      name, age
+    }
+
+    console.log("asdf+++++", newData, id)
+    try {
+
+      await db.collection('users').doc(id).update({ name, age, id: 5 })
+      res.json({
+        message: 'call by the PUT method ID:' + req.params.id,
+      });
+    } catch (error) {
+      throw new Error;
+    }
   })
-  .delete((req: Request, res: Response) => {
-    // ここデータ削除の処理を書く
-    res.json({
-      message: 'call by the DELETE method ID:' + req.params.id,
-    });
+  .delete(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      await db.collection('users').doc(id).delete();
+      res.json({
+        message: 'call by the DELETE method ID:' + req.params.id,
+      });
+    } catch (error) {
+      throw new Error;
+    }
   });
 
 module.exports = router;
